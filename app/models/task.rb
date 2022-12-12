e1e87b16ab82afd73461b2481f873c1952a29dd1
Ben Purinton <ben.purinton@gmail.com>
2022-12-12 05:31:46 +0000

    regen tasks with body as text

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,13 +3,11 @@
 # Table name: tasks
 #
 #  id         :integer          not null, primary key
-#  body       :string
+#  body       :text
 #  status     :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  user_id    :integer
 #
 class Task < ApplicationRecord
-
-  belongs_to :user, required: true, class_name: "User", foreign_key: "user_id", counter_cache: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_12_051349) do
+ActiveRecord::Schema.define(version: 2022_12_12_052846) do
 
   create_table "tasks", force: :cascade do |t|
     t.integer "user_id"
-    t.string "body"
+    t.text "body"
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
